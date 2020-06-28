43c37a30987a8bbf3032bbe323968bafe72c2829
Muradkhan0161 <murad.khan@northsouth.edu>
2020-06-28 22:48:11 +0600

    different routes

--- app.js
+++ app.js
@@ -2,14 +2,19 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('In the middleware!');
-  next(); // Allows the request to continue to the next middleware in line
+app.use('/',(req, res, next) => {
+  console.log('This will always runs first');
+  next();
 });
 
-app.use((req, res, next) => {
+app.use('/addProduct',(req, res, next) => {
+  console.log('Hey!');
+  res.send('<h1>This is "Add product page"</h1>');
+});
+
+app.use('/',(req, res, next) => {
   console.log('In another middleware!');
-  res.send('<h1>Hello from Express!</h1>');
+  res.send('<h1>Express is here to help</h1>');
 });
 
-app.listen(4444);
+app.listen(3333);
