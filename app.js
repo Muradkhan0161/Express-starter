f92e4cec4276f38de4c7d86b95ca40a10c3eb17f
Muradkhan0161 <murad.khan@northsouth.edu>
2020-06-28 19:58:51 +0600

    adding middlewar in express req,res,next

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const express = require('express')
 
 const app = express();
 
+app.use((req, res, next) => {
+    console.log('In the middle war');
+    next();
+})
+
+app.use((req, res, next) => {
+    console.log('Another middle war');
+})
+
 const server = http.createServer(app);
 
 server.listen(9000);
