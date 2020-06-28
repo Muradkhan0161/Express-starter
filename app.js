6f168eab5d9bf6ae3ec54b99e4bafc6359199766
Muradkhan0161 <murad.khan@northsouth.edu>
2020-06-28 21:13:16 +0600

    middleware works

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
-const http = require('http');
-
-const express = require('express')
+const express = require('express');
 
 const app = express();
 
 app.use((req, res, next) => {
-    console.log('In the middle war');
-    next();
-})
+  console.log('In the middleware!');
+  next(); // Allows the request to continue to the next middleware in line
+});
 
 app.use((req, res, next) => {
-    console.log('Another middle war');
-})
+  console.log('In another middleware!');
+  res.send('<h1>Hello from Express!</h1>');
+});
 
-const server = http.createServer(app);
-
-server.listen(9000);
+app.listen(4444);
