f22b4e94fc55568e3302a5a51e63e39c0e54497f
Muradkhan0161 <murad.khan@northsouth.edu>
2020-06-30 15:01:05 +0600

    adding helper function to navigate

--- app.js
+++ app.js
@@ -3,11 +3,9 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser'); 
 
-
 const app = express();
 
 const adminRoutes = require('./routes/admin');
-
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
--- routes/admin.js
+++ routes/admin.js
@@ -2,16 +2,18 @@ const path = require('path');
 
 const express = require('express');
 
+const rootDir = require('../util/path');
+
 const router = express.Router();
 
 router.get('/addProduct',(req, res, next) => {
-  res.sendFile(path.join(__dirname , '../' , 'views' , 'addProduct.html'));    
+  res.sendFile(path.join(rootDir , 'views' , 'addProduct.html'));    
 });
   
   router.post('/addProduct', (req, res, next) => {
     console.log(req.body);
-    res.redirect('/')
-  })
+    res.redirect('/');
+  });
 
 module.exports = router;
   
