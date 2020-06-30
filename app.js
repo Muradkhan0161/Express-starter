6d08d0fee2344496ea8a364ea4e9bd7eba9b11f8
Muradkhan0161 <murad.khan@northsouth.edu>
2020-06-30 09:59:11 +0600

    404 page added

--- app.js
+++ app.js
@@ -14,5 +14,8 @@ app.get('/favicon.ico', (req, res) => res.status(204));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>')
+})
 
 app.listen(3000);
