e4b5f31fe9abdbe2353dbada0c688ae3100c114a
Muradkhan0161 <murad.khan@northsouth.edu>
2020-07-02 10:25:06 +0600

    sharing data across req and users

--- routes/shop.js
+++ routes/shop.js
@@ -4,11 +4,14 @@ const express = require('express');
 
 const rootDir = require('../util/path')
 
+const adminData = require('./admin');
+
 const router = express.Router();
 
 router.get('/',(req, res, next) => {
     // console.log('In another middleware!',req.originalUrl);
-     res.sendFile(path.join(rootDir , 'views', 'shop.html'));
+    console.log('shop.js',  adminData.products);
+    res.sendFile(path.join(rootDir , 'views', 'shop.html'));
    });
 
 module.exports = router;
