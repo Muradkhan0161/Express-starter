ae1a611e1b54288ad03e8d35c05bd2ac34e9019d
Muradkhan0161 <murad.khan@northsouth.edu>
2020-07-04 10:16:34 +0600

    handlebars added

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser'); 
+const expressHbs = require('express-handlebars')
 
 const app = express();
 
-app.set('view engine','pug');
+app.engine('hbs' , expressHbs());
+app.set('view engine','hbs');
 app.set('views' , 'views');
 
 const adminData = require('./routes/admin');
@@ -21,7 +23,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render("404.pug", {pageTitle:'Tumi ke bhai'})
+    res.status(404).render("404", {pageTitle:'Tumi ke bhai'})
 });
 
 app.listen(3000);
